2d275e965e1ab8e1b83e303e1a36e58aed4f4a30
atanvarno <atanvarno@gmail.com>
2025-02-20 23:47:01 +0000

    fix(blink): Unfuck blink
    Signed-off-by: atanvarno <atanvarno@gmail.com>

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,25 +1,54 @@
 return {
     "saghen/blink.cmp",
     opts = {
+        appearance = {
+            nerd_font_variant = "normal",
+        },
+        completion = {
+            accept = { dot_repeat = false },
+            documentation = { auto_show = true, auto_show_delay_ms = 500 },
+            keyword = { range = "full" },
+            list = {
+                selection = {
+                    preselect = true,
+                    auto_insert = false,
+                },
+            },
+            menu = {
+                draw = {
+                    columns = {
+                        { "kind_icon" },
+                        { "label", "label_description", gap = 1 },
+                        { "source_name" },
+                    },
+                    components = {
+                        kind_icon = {
+                            ellipsis = false,
+                            text = function(ctx)
+                                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                                return kind_icon
+                            end,
+                            -- Optionally, you may also use the highlights from mini.icons
+                            highlight = function(ctx)
+                                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                                return hl
+                            end,
+                        },
+                    },
+                },
+            },
+        },
         keymap = {
+            preset = "none",
             ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
             ["<Esc>"] = { "cancel", "hide", "fallback" },
-            ["<Tab>"] = {
-                function(cmp)
-                    if cmp.snippet_active() then
-                        return cmp.accept()
-                    else
-                        return cmp.select_and_accept()
-                    end
-                end,
-                "snippet_forward",
-                "fallback",
-            },
+            ["<Tab>"] = { "accept", "fallback" },
             ["<S-Tab>"] = { "snippet_backward", "fallback" },
+            ["<C-Tab>"] = { "snippet_forward", "fallback" },
             ["<Up>"] = { "select_prev", "fallback" },
             ["<Down>"] = { "select_next", "fallback" },
-            ["<C-k>"] = { "scroll_documentation_up", "fallback" },
-            ["<C-j>"] = { "scroll_documentation_down", "fallback" },
+            ["<C-Up>"] = { "scroll_documentation_up", "fallback" },
+            ["<C-Down>"] = { "scroll_documentation_down", "fallback" },
         },
     },
 }
